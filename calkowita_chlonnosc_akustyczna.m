0a5742ce3c6d8b272d56090de982214b88146b40
itrail <38611732+itrail@users.noreply.github.com>
2020-09-07 20:02:51 +0200

    Usunięcie nie używanego skryptu

--- calkowita_chlonnosc_akustyczna.m
+++
@@ -1,6 +0,0 @@
-function A = calkowita_chlonnosc_akustyczna(w,l,h)
-    sufit = l*w*0.05;
-    sciany = 2 * (2.6*2*0.01) + 2*2.3*0.01 + ((2.3*2)-(2*0.8))*0.01;
-    drzwi = 2*0.8*0.08;
-    A = sufit + sciany + drzwi;
-end
